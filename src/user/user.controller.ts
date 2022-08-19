585929841ec280f5dd0aa064aac57139105e555f
mniang04 <mohametniang4@gmail.com>
2022-08-19 15:35:26 +0000

    Api work, swagger cool

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,22 +1,35 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, NotFoundException, Param, Post, Query } from '@nestjs/common';
+import { ApiCreatedResponse, ApiNotFoundResponse, ApiOkResponse, ApiQuery, ApiTags } from '@nestjs/swagger';
+import { type } from 'os';
 import { CreateUserDTO } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
 import { UserService } from './user.service';
 
+@ApiTags ('users')
 @Controller('user')
 export class UserController {
     constructor(private userServise: UserService) {}
 
+    @ApiOkResponse({ type: User, isArray: true })
+    @ApiQuery({ name: 'name' , required: false })
     @Get()
-    getUsers(): any {
-        return this.userServise.findALL();
-    }
-    @Get(':id')
-    getUserById(@Param('id') id: string): any {
-        return this.userServise.findById(Number(id));
+    getUsers(@Query('name') name?: string): User[] {
+        return this.userServise.findALL(name );
     }
 
+    @ApiNotFoundResponse({ type: User, description: 'The User' })
+    @Get(':id')
+    getUserById(@Param('id') id: string): User {
+        const user = this.userServise.findById(Number(id));
+        if (!user) {
+            throw new NotFoundException
+        }
+        return user
+    }
+
+    @ApiCreatedResponse({ type: User })
     @Post()
-     createUser(@Body() body: CreateUserDTO): any {
+     createUser(@Body() body: CreateUserDTO): User {
         return this.userServise.createUser(body)
      }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,22 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDTO } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
-    private users: any = [{ id: 0, name: "Mohamed" }, { id: 1, name: "Moustapha" }]
+    private users: User[] = [
+        { id: 0, name: "Mohamed" }, 
+        { id: 1, name: "Moustapha" },
+        { id: 2, name: "Nogaye " }
+    ]
 
-    findALL() {
+    findALL(name?: string): User[] {
+
+        if (name) {
+            return this.users.filter((user: any) => user.name === name);
+        }
         return this.users;
     }
 
-    findById(userId: number) {
+    findById(userId: number): User {
         return this.users.find((user: any) => user.id === userId);
     }
 
-    createUser(createUserDTO: CreateUserDTO) {
+    createUser(createUserDTO: CreateUserDTO): User {
         const newUser = { id: Date.now(), ...createUserDTO }
         this.users.push(newUser)
 
-        return newUser
+        return newUser 
     }
 } 
